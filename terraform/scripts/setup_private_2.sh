1f6cf68f196a2e4b0626f5e4ff3998477884fa15
Julian Felipe Eraso Adarme <erasojulian155@gmail.com>
2026-05-27 00:13:28 +0200

    Update setup_private_2.sh

--- terraform/scripts/setup_private_2.sh
+++ terraform/scripts/setup_private_2.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 dnf update -y
 dnf install -y docker
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo systemctl start docker
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+systemctl start docker
 systemctl enable --now docker
 mkdir -p /home/ec2-user/nextcloud && cd /home/ec2-user/nextcloud
 
 cat <<EOF > docker-compose.yml
 services:
+
   app:
     image: nextcloud:latest
-    ports: ["80:80"]
+
+    container_name: nextcloud
+
+    ports:
+      - "80:80"
+
+    volumes:
+      - "./nextcloud:/var/www/html"
+
     environment:
-      - POSTGRES_HOST=10.0.2.100 # IP de la primera máquina privada
+      - POSTGRES_HOST=10.0.2.100
       - POSTGRES_DB=nextcloud
       - POSTGRES_USER=nextcloud
       - POSTGRES_PASSWORD=nc_pass
-      - NEXTCLOUD_TRUSTED_DOMAINS=*
-       # --- CONTROL DE ACCESO ESTRICTO ---
-      - TRUSTED_PROXIES=10.0.1.0/24  # Solo acepta peticiones que vengan de la subred pública (Nginx)
+
+      - NEXTCLOUD_TRUSTED_DOMAINS=34.238.43.82
+
+      - TRUSTED_PROXIES=10.0.1.0/24
+
+      - OVERWRITEHOST=34.238.43.82
+      - OVERWRITEPROTOCOL=http
+      - OVERWRITEWEBROOT=/nextcloud
+
+      - APACHE_DISABLE_REWRITE_IP=1
+
+    restart: unless-stopped
+
 EOF
-docker compose up -d
+docker-compose up -d
