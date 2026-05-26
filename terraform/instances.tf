8e9260c3c9e0ac4df0114f4073d299ec8e834dfc
Julian Felipe Eraso Adarme <erasojulian155@gmail.com>
2026-05-27 00:14:07 +0200

    Update instances.tf

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,7 +1,7 @@
 # Instancia Pública (Nginx, Gitea, VSCode)
 resource "aws_instance" "public_node" {
   ami                    = "ami-02dfbd4ff395f2a1b"
-  instance_type          = "t3.small"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public.id
   user_data              = file("scripts/setup_public.sh")
   vpc_security_group_ids = [aws_security_group.allow_web.id]
@@ -11,7 +11,7 @@ resource "aws_instance" "public_node" {
 # Instancia Privada 1 (Base de Datos Postgres Principal)
 resource "aws_instance" "private_node_1" {
   ami                    = "ami-02dfbd4ff395f2a1b"
-  instance_type          = "t3.small"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.private.id
   private_ip             = "10.0.2.100"
   user_data              = file("scripts/setup_private.sh")
@@ -22,7 +22,7 @@ resource "aws_instance" "private_node_1" {
 # Instancia Privada 2 (Nextcloud Nodo 1 - Se conecta a la BD de la Instancia 1)
 resource "aws_instance" "private_node_2" {
   ami                    = "ami-02dfbd4ff395f2a1b"
-  instance_type          = "t3.small"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.private.id
   private_ip             = "10.0.2.110"
   user_data              = file("scripts/setup_private_2.sh")
@@ -32,10 +32,10 @@ resource "aws_instance" "private_node_2" {
 # Instancia Privada 2 (Nextcloud Nodo 2 - Se conecta a la BD de la Instancia 1)
 resource "aws_instance" "private_node_3" {
   ami                    = "ami-02dfbd4ff395f2a1b"
-  instance_type          = "t3.small"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.private.id
   private_ip             = "10.0.2.120"
-  user_data              = file("scripts/setup_private_3.sh")
+  user_data              = file("scripts/setup_private_2.sh")
   vpc_security_group_ids = [aws_security_group.allow_internal.id]
   key_name               = "vockey"
 }
