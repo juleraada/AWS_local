bd412c463a9dbc695f4f8e78001fd69ce60447a9
Julian Felipe Eraso Adarme <erasojulian155@gmail.com>
2026-05-27 00:15:04 +0200

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,11 +8,13 @@ resource "aws_internet_gateway" "igw" { vpc_id = aws_vpc.main.id }
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
+  availability_zone       = "us-east-1a"
   cidr_block = "10.0.2.0/24"
 }
 
